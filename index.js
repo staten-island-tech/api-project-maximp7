98f2a2a42be88ba0359c78c24216b6292ca8ddbe
maximp7 <144476708+maximp7@users.noreply.github.com>
2024-01-01 21:31:04 -0500

    ds

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
+import "../styles/style.css";
+
 const URL = "https://deckofcardsapi.com/api/deck/new/draw/?count=52";
+
+const DOMSelectors = {
+  allb: document.querySelector("#all"),
+  Heartsb: document.querySelector("#Hearts"),
+  Spadesb: document.querySelector("#Spades"),
+  Cloverb: document.querySelector("#Clubs"),
+  Diamondb: document.querySelector("#Diamonds"),
+  Royalsb: document.querySelector("#Royals"),
+  cardcontainer: document.querySelector(".card-container"),
+};
+
+
+
+function Displaying() {
+     fetch(URL)
+       .then((response) => response.json())
+       .then((data) => {
+
+
+         data.cards.forEach((element)=> document.querySelector(".card-container").insertAdjacentHTML(    "beforeend",
+         `<div class="cards">
+           <div class="card-image">
+             <img class="pic" src="${element.image}">
+           </div>
+           <div class="card-content">
+             <h3>${element.value}</h3>
+             <p>${element.suit}</p>
+           </div>
+       </div>`)
+       
+       )})
+       .catch((error) => console.log(error));
+   }
+Displaying();
