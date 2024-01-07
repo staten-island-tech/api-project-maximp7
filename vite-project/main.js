114189b30cf9357b34270865b391b8e91361f502
maximp7 <144476708+maximp7@users.noreply.github.com>
2024-01-06 23:14:34 -0500

    drfg

--- vite-project/main.js
+++ vite-project/main.js
@@ -1,4 +1,4 @@
-import styles from "./css/style.css";
+import "/style.css";
 
 
 const URL = "https://deckofcardsapi.com/api/deck/new/draw/?count=52";
@@ -36,3 +36,154 @@ function Displaying() {
        .catch((error) => console.log(error));
    }
 Displaying();
+
+
+
+function displayHearts() {
+
+fetch(URL)
+.then((response) => response.json())
+.then((datah) => {
+
+  datah.cards.filter((element) => element.suit === "HEARTS")
+  .forEach((element) => document.querySelector(".card-container").insertAdjacentHTML(    "beforeend",
+  `<div class="cards">
+    <div class="card-image">
+      <img class="pic" src="${element.image}">
+    </div>
+    <div class="card-content">
+      <h3>${element.value}</h3>
+      <p>${element.suit}</p>
+    </div>
+</div>`))
+})}
+  
+function displaySpades(){
+fetch(URL)
+.then((response) => response.json())
+.then((datah) => {
+
+  datah.cards.filter((element) => element.suit === "SPADES")
+  .forEach((element) => document.querySelector(".card-container").insertAdjacentHTML(    "beforeend",
+  `<div class="cards">
+    <div class="card-image">
+      <img class="pic" src="${element.image}">
+    </div>
+    <div class="card-content">
+      <h3>${element.value}</h3>
+      <p>${element.suit}</p>
+    </div>
+</div>`))
+})}
+
+function displayClubs() {
+  fetch(URL)
+  .then((response) => response.json())
+  .then((datah) => {
+  
+    datah.cards.filter((element) => element.suit === "CLUBS")
+    .forEach((element) => document.querySelector(".card-container").insertAdjacentHTML(    "beforeend",
+    `<div class="cards">
+      <div class="card-image">
+        <img class="pic" src="${element.image}">
+      </div>
+      <div class="card-content">
+        <h3>${element.value}</h3>
+        <p>${element.suit}</p>
+      </div>
+  </div>`))
+  })}
+function displayDiamonds() {
+  fetch(URL)
+  .then((response) => response.json())
+  .then((datah) => {
+  
+    datah.cards.filter((element) => element.suit === "DIAMONDS")
+    .forEach((element) => document.querySelector(".card-container").insertAdjacentHTML(    "beforeend",
+    `<div class="cards">
+      <div class="card-image">
+        <img class="pic" src="${element.image}">
+      </div>
+      <div class="card-content">
+        <h3>${element.value}</h3>
+        <p>${element.suit}</p>
+      </div>
+  </div>`))
+  })}
+
+function displayK() {
+  fetch(URL)
+  .then((response) => response.json())
+  .then((datah) => {
+  
+    datah.cards.filter((element) => 
+    element.value.includes("K"))
+    .forEach((element) => document.querySelector(".card-container").insertAdjacentHTML(    "beforeend",
+    `<div class="cards">
+      <div class="card-image">
+        <img class="pic" src="${element.image}">
+      </div>
+      <div class="card-content">
+        <h3>${element.value}</h3>
+        <p>${element.suit}</p>
+      </div>
+  </div>`))
+  })}
+
+  function displayE() {
+    fetch(URL)
+    .then((response) => response.json())
+    .then((datah) => {
+    
+      datah.cards.filter((element) => 
+      element.value.includes("E"))
+      .forEach((element) => document.querySelector(".card-container").insertAdjacentHTML(    "beforeend",
+      `<div class="cards">
+        <div class="card-image">
+          <img class="pic" src="${element.image}">
+        </div>
+        <div class="card-content">
+          <h3>${element.value}</h3>
+          <p>${element.suit}</p>
+        </div>
+    </div>`))
+    })}
+
+function erase() {
+  DOMSelectors.cardcontainer.innerHTML = "";
+}
+
+function Allp() {
+  erase();
+  Displaying();
+}
+
+function Heartsp() {
+  erase();
+  displayHearts();
+}
+
+function Spadesp() {
+  erase();
+  displaySpades();
+}
+function Clubsp() {
+  erase();
+  displayClubs();
+}
+function Diamondp() {
+  erase();
+  displayDiamonds();
+}
+function Royalsp() {
+  erase();
+  displayK();
+  displayE();
+}
+
+DOMSelectors.allb.addEventListener("click", Allp);
+DOMSelectors.Heartsb.addEventListener("click", Heartsp);
+DOMSelectors.Spadesb.addEventListener("click", Spadesp);
+DOMSelectors.Cloverb.addEventListener("click", Clubsp);
+DOMSelectors.Diamondb.addEventListener("click", Diamondp);
+DOMSelectors.Royalsb.addEventListener("click", Royalsp);
